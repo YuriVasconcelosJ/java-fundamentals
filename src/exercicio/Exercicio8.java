48420c9f1696ee269d94441cb7cfecb45e2b616b
YuriVasconcelosj <yurivasconcelosavatar@gmail.com>
2025-04-26 16:01:04 -0300

    Finish

--- src/exercicio/Exercicio8.java
+++ src/exercicio/Exercicio8.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 public class Exercicio8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // Variable Declaration 
-        int code, code2, partNumbers, partNumbers2 ;
-        double valueUnit, valueUnit2, result;
+        // Variable Declaration
+        int code, code2, partNumbers, partNumbers2;
+        float valueUnit, valueUnit2, result;
 
         System.out.println("Enter the code, part of numbers, value unit: ");
         code = sc.nextInt();
         partNumbers = sc.nextInt();
         valueUnit = sc.nextInt();
+        code2 = sc.nextInt();
+        partNumbers2 = sc.nextInt();
+        valueUnit2 = sc.nextInt();
 
-        
+        float sum = ((partNumbers * valueUnit) + (partNumbers2 * valueUnit2));
 
-        
+        System.out.printf("Value pay: R$ %.2f", sum);
     }
-    
+
 }
